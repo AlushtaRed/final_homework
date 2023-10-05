11f7fc8d85aa30f55c5093e7595f3abf8177b8fb
Igor <ygarik@mail.ru>
2023-10-05 20:43:21 +0300

    написал программу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿string [] array = new string[4];
+void FillArray(string[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    Console.WriteLine(String.Join(", ", array));
+}
+int ArrayLength(string[]array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+     string text = array[i]; int length = text.Length;
+     if(length <= 3)
+     {
+        count +=  1;
+     }   
+     
+    }
+    return count;
+}
+FillArray(array);
+ArrayLength(array);
+int length = ArrayLength(array);
+
+string[]newarray = new string[length];
+void FillNewArray(string[]newarray)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        string text = array[i]; int length = text.Length;
+        if(length<=3)
+        {
+            newarray[i]=array[i];
+        }
+    }
+    Console.WriteLine(String.Join(", ", newarray));
+}
+
+FillNewArray(newarray);
